e0a7217b06613b9462bbfa5bf3f763882714784a
Aaró Sibaja Villalobos <asvaaron@gmail.com>
2018-09-23 19:16:28 -0600

    Add class for Redis Connection
    Add package redis
    Add Redis to requeriments file
    Add ide files to .gitignore

---
+++ source/__init__.py
@@ -0,0 +1,6 @@
+from .redis import RedisConnection
+
+
+
+
+
---
+++ source/redis/redis_connection.py
@@ -0,0 +1,39 @@
+import redis
+
+
+class RedisConnection:
+
+    # Default port: 6378
+    # Default host: localhost
+    def __init__(self, host='127.0.0.1', db=0, port=6378):
+        # Configure Redis Connection
+        try:
+            self.redis_connection = redis.StrictRedis(
+                host=host,
+                port=port,
+                db=db
+            )
+        except IOError as e:
+            print
+            "I/O error({0}): {1}".format(e.errno, e.strerror)
+        except ConnectionError:
+            print
+            "Could not convert data to an integer."
+
+
+# # Set my port connection
+# redis = RedisConnection(port=6379)
+# redis.redis_connection.set('foo', 'bar')
+# print(redis.redis_connection.get('foo'))
+#
+# redis.redis_connection.hmset("route:lele", {
+#                                     'hello':['sdas','dasdda'],
+#                                     'lala':15,
+#                                     'caca':18
+#                                  }
+#                              )
+# #
+#
+# # Block Chain List
+# for i in range(0, 10):
+#     print(redis.redis_connection.brpop({'comments'}, timeout=100))
