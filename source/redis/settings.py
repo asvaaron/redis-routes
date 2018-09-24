2f78fa3a8a5f8ea78f79bfbdf78c5b583f387ee7
Aaró Sibaja Villalobos <asvaaron@gmail.com>
2018-09-23 20:13:06 -0600

    Add python dotenv
    Add .template.env file
    Add .env keys, settings.py

--- source/redis/settings.py
+++ source/redis/settings.py
@@ -1,2 +1,11 @@
 from dotenv import load_dotenv
+import os
+
+# LOAD ENV file
 load_dotenv()
+
+
+REDIS_PASSWORD=os.getenv('REDIS_PASSWORD')
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_DEFAULT_DB = os.getenv('REDIS_DEFAULT_DB')
+REDIS_PORT = os.getenv('REDIS_PORT')
