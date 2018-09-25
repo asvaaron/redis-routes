5fcc068203e1926ab80613260620739ff54aa2c0
Aaró Sibaja Villalobos <asvaaron@gmail.com>
2018-09-24 18:21:49 -0600

    Changes in Routes file
    Refractor code, routes.py
    Create function for retriving hash key, routes.py

--- source/redis-manager/routes.py
+++ source/redis-manager/routes.py
@@ -10,13 +10,24 @@ class Routes ():
 
 
     def insert_new_route(self, route_name, places, times):
-        self.redis.redis_connection.hmset(self.root_key_name+':'+str(route_name),
-                                          {
-                                              'places': places,
-                                              'times': times
-                                  }
-                              )
+        self.redis.redis_connection.hmset(
+            self.root_key_name+':'+str(route_name),
+            {
+                'places': places,
+                'times': times
+            }
+        )
 
+
+    def get_route(self,route_name):
+        return self.redis.redis_connection.hmget(
+            self.root_key_name+':'+str(route_name),
+            [
+                'places',
+                'times'
+            ]
+        )
 
 rout = Routes()
 rout.insert_new_route('lala', ['hello', 'asdas', 'dassd'], ['11/13/18', '15/12/18'])
+print(rout.get_route('lala'))
