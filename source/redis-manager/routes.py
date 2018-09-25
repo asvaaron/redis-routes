e4ba3d334782c96194195fad7c975e151a8d0677
Aaró Sibaja Villalobos <asvaaron@gmail.com>
2018-09-24 18:42:36 -0600

    Changes in Routes file
    Refractor code, routes.py
    Add comments doc to functions routes.py

--- source/redis-manager/routes.py
+++ source/redis-manager/routes.py
@@ -6,14 +6,25 @@ class Routes ():
 
     def __init__(self):
         self.redis = RedisConnection(port=REDIS_PORT)
-        self.root_key_name= 'route'
+        self.root_key_name = 'route'
 
 
     def route_hash_name(self, route_name):
+        """
+        Return string with the route name using prefix
+        :param route_name: name of the route
+        :return: route name parsed
+        """
         return self.root_key_name+':'+str(route_name)
 
 
     def insert_new_route(self, route_name, places, times):
+        """
+        Insert new hash route in redis database
+        :param route_name: name of the new route
+        :param places: all available places
+        :param times: departure time for each place
+        """
         self.redis.redis_connection.hmset(
             self.route_hash_name(route_name),
             {
@@ -24,12 +35,22 @@ class Routes ():
 
 
     def routes_keys_list(self, route_name):
+        """
+        Return all the key values from a route
+        :param route_name:
+        :return: key values
+        """
         return self.redis.redis_connection.hkeys(
             self.route_hash_name(route_name)
         )
 
 
     def get_route(self,route_name):
+        """
+        Return an array with all the key values
+        :param route_name:
+        :return: array
+        """
         return self.redis.redis_connection.hmget(
             self.route_hash_name(route_name),
             [
@@ -40,4 +61,4 @@ class Routes ():
 
 rout = Routes()
 rout.insert_new_route('lala', ['hello', 'asdas', 'dassd'], ['11/13/18', '15/12/18'])
-print(rout.routes_keys_list('lala'))
+print(rout.get_route('lala'))
